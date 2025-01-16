3d31560ab470764e0f6836e7d5b91548c280c1c7
Luca Vignali <lucavigno2003@gmail.com>
2025-01-16 15:00:44 +0100

    +confusion matrix to train and eval

--- src/train_evaluate.py
+++ src/train_evaluate.py
@@ -1,9 +1,13 @@
 import torch
 from torch.utils.tensorboard import SummaryWriter
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
 
 def train_model(model, train_dataloader, optimizer, criterion, num_epochs, config):
     writer = SummaryWriter(config['log_dir'])
     model.train()
+    all_labels = []
+    all_predictions = []
     for epoch in range(num_epochs):
         epoch_loss = 0
         correct_predictions = 0
@@ -21,12 +25,21 @@ def train_model(model, train_dataloader, optimizer, criterion, num_epochs, confi
             correct_predictions += (predicted == labels_max).sum().item()
             total_predictions += labels.size(0)
 
+            all_labels.extend(labels_max.cpu().numpy())
+            all_predictions.extend(predicted.cpu().numpy())
+
         avg_loss = epoch_loss / len(train_dataloader)
         accuracy = correct_predictions / total_predictions * 100
 
         writer.add_scalar('Loss/train', avg_loss, epoch)
         writer.add_scalar('Accuracy/train', accuracy, epoch)
         print(f'Epoch {epoch+1}/{num_epochs}, Loss: {avg_loss}, Accuracy: {accuracy}%')
+
+    # Confusion Matrix
+    cm = confusion_matrix(all_labels, all_predictions)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+    disp.plot()
+    plt.show()
 
     writer.close()
 
@@ -35,6 +48,8 @@ def evaluate_model(model, eval_dataloader, criterion):
     total_loss = 0
     correct_predictions = 0
     total_predictions = 0
+    all_labels = []
+    all_predictions = []
     with torch.no_grad():
         for inputs, labels in eval_dataloader:
             outputs = model(inputs)
@@ -46,9 +61,19 @@ def evaluate_model(model, eval_dataloader, criterion):
             correct_predictions += (predicted == labels_max).sum().item()
             total_predictions += labels.size(0)
 
+            all_labels.extend(labels_max.cpu().numpy())
+            all_predictions.extend(predicted.cpu().numpy())
+
     avg_loss = total_loss / len(eval_dataloader)
     accuracy = correct_predictions / total_predictions * 100
     print(f'Evaluation Loss: {avg_loss}, Accuracy: {accuracy}%')
+
+    # Confusion Matrix
+    cm = confusion_matrix(all_labels, all_predictions)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+    disp.plot()
+    plt.show()
+
     return avg_loss, accuracy
 
 def test_model(model, test_dataloader, criterion):
@@ -56,6 +81,8 @@ def test_model(model, test_dataloader, criterion):
     total_loss = 0
     correct_predictions = 0
     total_predictions = 0
+    all_labels = []
+    all_predictions = []
     with torch.no_grad():
         for inputs, labels in test_dataloader:
             outputs = model(inputs)
@@ -67,7 +94,11 @@ def test_model(model, test_dataloader, criterion):
             correct_predictions += (predicted == labels_max).sum().item()
             total_predictions += labels.size(0)
 
+            all_labels.extend(labels_max.cpu().numpy())
+            all_predictions.extend(predicted.cpu().numpy())
+
     avg_loss = total_loss / len(test_dataloader)
     accuracy = correct_predictions / total_predictions * 100
     print(f'Test Loss: {avg_loss}, Accuracy: {accuracy}%')
+
     return avg_loss, accuracy

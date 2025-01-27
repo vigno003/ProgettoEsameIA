b0c09b40a2fced546d82237d1bd7c6dbb76c6534
Luca Vignali <lucavigno2003@gmail.com>
2025-01-27 17:08:05 +0100

    removed 2 comments

--- src/create_graphs.py
+++ src/create_graphs.py
@@ -3,8 +3,6 @@ from torch.utils.tensorboard import SummaryWriter
 
 def create_graphs(log_dir='runs/meteo_model'):
     writer = SummaryWriter(log_dir)
-    # Assuming you have already logged the data during training
-    # You can use TensorBoard to visualize the graphs
     writer.flush()
     writer.close()
     print(f"Graphs can be viewed using TensorBoard with the log directory: {log_dir}")
